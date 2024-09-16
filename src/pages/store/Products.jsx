7a9339cfbaf2a5fb2cbb7478b339228a40c2fe53
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-16 17:15:41 +0300

    Refactor: Add react-select dependency and create Products component

--- src/pages/store/Products.jsx
+++ src/pages/store/Products.jsx
@@ -1,4 +1,5 @@
 import Select from "react-select";
+import { ProductCard } from "../Home/ProductCard";
 
 export const Products = () => {
   const options = [
@@ -7,14 +8,14 @@ export const Products = () => {
     { value: "men", label: "Men" },
   ];
 
-  // Custom styles for react-select
   const customStyles = {
     control: (provided) => ({
       ...provided,
       backgroundColor: "transparent",
-      borderColor: "#A0522D", // clayBrown color
+      borderColor: "#A0522D",
       padding: "10px",
       textAlign: "center",
+      outLine: "none",
     }),
     option: (provided, { isFocused }) => ({
       ...provided,
@@ -31,6 +32,149 @@ export const Products = () => {
     }),
   };
 
+  const products = [
+    {
+      name: "Revitalizing Face Cream",
+      price: 34.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/11751106/pexels-photo-11751106.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Hydrating Facial Serum",
+      price: 29.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/5517064/pexels-photo-5517064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Nourishing Body Lotion",
+      price: 24.99,
+      category: "Body Care",
+      image:
+        "https://images.pexels.com/photos/12352238/pexels-photo-12352238.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Soothing Eye Cream",
+      price: 19.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/12035707/pexels-photo-12035707.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Exfoliating Scrub",
+      price: 27.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/8128062/pexels-photo-8128062.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Deep Cleanse Face Mask",
+      price: 22.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/3373730/pexels-photo-3373730.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Moisturizing Hand Cream",
+      price: 18.99,
+      category: "Body Care",
+      image:
+        "https://images.pexels.com/photos/4699180/pexels-photo-4699180.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Refreshing Facial Mist",
+      price: 16.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/6808150/pexels-photo-6808150.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      name: "Anti-Aging Night Cream",
+      price: 39.99,
+      category: "Skincare",
+      image:
+        "https://images.pexels.com/photos/5128075/pexels-photo-5128075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Calming Lip Balm",
+      price: 12.99,
+      category: "Lip Care",
+      image:
+        "https://images.pexels.com/photos/8128071/pexels-photo-8128071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
   return (
     <div className="my-8 font-playfair">
       <div className="w-full flex items-center justify-between">
@@ -41,6 +185,11 @@ export const Products = () => {
           defaultValue={options[0]}
           isSearchable={false}
         />
+      </div>
+      <div>
+        {products.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
       </div>
     </div>
   );
