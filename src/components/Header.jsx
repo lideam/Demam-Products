c3aa7d8b14e3fe7c1442781d9fe323b0efbcd8af
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-16 16:34:44 +0300

    Refactor: Update Header component to use Link for the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <div className="font-playfair flex flex-col gap-4 w-full p-4 border-b border-gray-500">
       <div className="w-full text-[#4a4a4a] flex flex-wrap items-center justify-between">
         <span></span>
-        <h1 className="text-3xl font-extrabold cursor-pointer">SOFT BEAUTY</h1>
+        <Link to="/" className="text-3xl font-extrabold cursor-pointer">
+          SOFT BEAUTY
+        </Link>
         <div className="flex gap-2 cursor-pointer text-xl">
           <i className="fa fa-user"></i>
           <i className="fa fa-search"></i>
