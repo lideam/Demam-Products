1644c0094db60cf0bd1025570b42f6ecd8f3f721
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-17 20:54:06 +0300

    Refactor: Update ProductDetail component with multiple product images and dynamic selection

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -1,28 +1,40 @@
 import { useState } from "react";
 
 export const ProductDetail = () => {
-  const [image, setImage] = useState(
-    "https://imgs.search.brave.com/V4PHzJCCbTx1cSKAtvrBASPRmozfMkHI5yUJxhoJIuU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTZfMzUweC5q/cGc"
-  );
+  const [image, setImage] = useState([
+    1,
+    "https://imgs.search.brave.com/V4PHzJCCbTx1cSKAtvrBASPRmozfMkHI5yUJxhoJIuU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTZfMzUweC5q/cGc",
+  ]);
 
+  const images = {
+    1: {
+      id: 1,
+      url: "https://imgs.search.brave.com/V4PHzJCCbTx1cSKAtvrBASPRmozfMkHI5yUJxhoJIuU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTZfMzUweC5q/cGc",
+    },
+    2: {
+      id: 2,
+      url: "https://imgs.search.brave.com/gm_tR0EDe3WHH8OG3ToBtoWicxntR5FVvuePShtmSLE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTdfMzUweC5q/cGc",
+    },
+  };
 
   return (
     <div className="my-8 mx-12 flex justify-around gap-12">
       <div className="flex gap-2">
         <div className="flex flex-col gap-4">
-          <img
-            src="https://imgs.search.brave.com/gm_tR0EDe3WHH8OG3ToBtoWicxntR5FVvuePShtmSLE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTdfMzUweC5q/cGc"
-            alt=""
-            className="flex-shrink-0 w-80 h-40 p-2 cursor-pointer  border-4 border-clayBrown"
-          />
-          <img
-            src=""
-            alt=""
-          />
+          {Object.values(images).map((i, index) => (
+            <img
+              src={i.url}
+              key={index}
+              className={`flex-shrink-0 w-80 h-40 p-2 cursor-pointer ${
+                image[0] == i.id && "border-4 border-clayBrown"
+              }`}
+              onClick={() => setImage([i.id, i.url])}
+            />
+          ))}
         </div>
         <div>
           <img
-            src={image}
+            src={image[1]}
             alt=""
             className=" flex-shrink-0 w-[1000px] h-[600px]"
           />
