3be25f78ba9230db8cb0c27d88d85c5835d4f575
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-17 20:18:30 +0300

    getting started with the product dertail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { Home } from "./Home/Home";
-// export { ProductDetail } from "./Product/ProductDetail";
-export { Auth } from "./Auth/Auth";
+export { ProductDetail } from "./Product/ProductDetail";
 export { Store } from "./store/Store";
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -14,6 +14,7 @@ export const MainRoutes = () => {
           <Routes>
             <Route path="*" element={<Home />} />
             <Route path="/store" element={<Store />} />
+            <Route path="/store" element={<Store />} />
           </Routes>
           <Footer />
         </Suspense>
