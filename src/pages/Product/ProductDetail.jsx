38e377644b0518d26beaca96c80f7523f14fc3d0
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-17 20:28:23 +0300

    Refactor: Update ProductDetail component with product information and styling

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -1,5 +1,46 @@
 export const ProductDetail = () => {
   return (
-    <div>ProductDetail</div>
-  )
-}
+    <div className="my-8 mx-12">
+      <div>
+        <div>
+          <img
+            src="https://imgs.search.brave.com/gm_tR0EDe3WHH8OG3ToBtoWicxntR5FVvuePShtmSLE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTdfMzUweC5q/cGc"
+            alt=""
+          />
+          <img
+            src="https://imgs.search.brave.com/V4PHzJCCbTx1cSKAtvrBASPRmozfMkHI5yUJxhoJIuU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTZfMzUweC5q/cGc"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://imgs.search.brave.com/V4PHzJCCbTx1cSKAtvrBASPRmozfMkHI5yUJxhoJIuU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFnaXJsdXNhLmNv/bS9jZG4vc2hvcC9w/cm9kdWN0cy9MQUdf/cHJvX3ByaW1lcl9H/RUIxOTZfMzUweC5q/cGc"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <header>Organic Cotton T-Shirt</header>
+        <ul>
+          <li>Clothing</li>
+          <li>Men’s Apparel</li>
+          <li>Sustainable Fashion</li>
+          <li>Eco-Friendly Products</li>
+        </ul>
+        <p>
+          Crafted from 100% organic cotton, this t-shirt offers a soft,
+          breathable fit for all-day comfort. The classic crew neck design is
+          both stylish and versatile, perfect for casual wear or layering.
+          Available in a range of earth-tone colors, it’s made with
+          sustainability in mind, making it an ideal choice for eco-conscious
+          shoppers.
+        </p>
+        <b>$29.99</b>
+        <div>
+          <input type="number" placeholder="Quantity" />
+          <button>Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
