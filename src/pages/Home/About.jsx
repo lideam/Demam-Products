22cee44d66a8531810b5ab071bc728eda23d2e17
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-19 10:05:45 +0300

    Refactor: Update Home component with improved styling and dynamic category selection

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -2,9 +2,9 @@ import Girl from "../../assets/about.png"
 
 export const About = () => {
   return (
-    <div className="bg-[#59403b] mt-[-40px] h-[600px] text-[#b1a9a8] flex flex-wrap justify-between font-playfair items-center">
-      <div className="p-32 w-[900px] flex flex-col gap-4">
-        <h1 className="text-4xl font-bold">
+    <div className="bg-[#59403b] mt-[-40px] h-[600px] text-[#b1a9a8] flex flex-col-reverse md:flex-row justify-between font-playfair items-center">
+      <div className=" p-4 md:p-32 w-screen md:w-[900px] flex flex-col gap-4">
+        <h1 className="text-2xl md:text-4xl font-bold">
           Discover our premium cosmetics and skincare products designed to
           enhance your natural beauty.
         </h1>
@@ -18,7 +18,7 @@ export const About = () => {
           excellence.
         </p>
       </div>
-      <img src={Girl} alt="" className="h-full w-[500px]" />
+      <img src={Girl} alt="" className="h-full w-56 self-end md:w-[500px]" />
     </div>
   );
 }
