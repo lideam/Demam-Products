97621da212937d36ce8b779b54c0653349fa5cc9
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-03 17:27:32 +0300

    Refactor: Add AdminRoutes component to handle admin routes

---
+++ Frontend/src/routes/AdminRoutes.jsx
@@ -0,0 +1,12 @@
+import { div } from "framer-motion/client";
+import { Route, Routes } from "react-router-dom";
+
+export const AdminRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element />
+      </Routes>
+    </div>
+  );
+};
