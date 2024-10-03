2d49a6c0bc42e48d9465038a76a51047807bf199
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-03 18:56:19 +0300

    Refactor: Update AuthProvider and Login.jsx in AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -19,32 +19,8 @@ export const AuthProvider = ({ children }) => {
       ? jwtDecode(localStorage.getItem("authTokens"))
       : null
   );
-  const [myprofile, setProfile] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (authTokens && user) {
-      const id = user.user_id;
-      const getProfile = async () => {
-        setLoading(true);
-        try {
-          const token = authTokens ? authTokens.access : null;
-          const response = await axios.get(`${backendUrl}api/admin/me`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          setProfile(response.data[0]);
-        } catch (error) {
-          toast.error("Error fetching profile data");
-        } finally {
-          setLoading(false);
-        }
-      };
-      getProfile();
-    }
-  }, [authTokens, user]);
 
   let loginUser = async (ph, pass) => {
     try {
@@ -52,19 +28,18 @@ export const AuthProvider = ({ children }) => {
       let response = await axios.post(
         `${backendUrl}api/admin/login`,
         {
-          phone: ph,
+          phoneNumber: ph,
           password: pass,
         },
         { timeout: 20000 }
       );
-
       if (response.status === 200) {
         const data = response.data;
-        setAuthTokens(data);
-        setUser(jwtDecode(data.access));
-        localStorage.setItem("authTokens", JSON.stringify(data));
+        setAuthTokens(data.token);
+        setUser(data.data);
+        localStorage.setItem("authTokens", JSON.stringify(data.token));
         toast.success("Logged in successfully");
-        navigate("/");
+        navigate("/superadmin");
       } else {
         toast.error(response.data.detail);
       }
@@ -88,7 +63,6 @@ export const AuthProvider = ({ children }) => {
     user,
     loginUser,
     logoutUser,
-    myprofile,
     authTokens,
     loading,
     setLoading,
