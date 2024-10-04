df2dc239ad1fdbe3688b49dbe8db9dde2abeb22c
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-04 13:02:44 +0300

    Refactor Card component styles in Admin page

--- Frontend/src/pages/Admin/Card.jsx
+++ Frontend/src/pages/Admin/Card.jsx
@@ -1,7 +1,7 @@
 export const Card = ({text, className, name}) => {
   return (
     <div className="w-full p-2 lg:w-1/4 md:w-1/2">
-      <div className="flex flex-col px-6 py-10 overflow-hidden hover:bg-gradient-to-br hover:from-clayBrown hover:to-sandyBeige text-white bg-gradient-to-br from-sandyBeige to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
+      <div className="flex flex-col px-6 py-10 overflow-hidden hover:bg-gradient-to-br hover:from-clayBrown hover:to-sandyBeige text-gray-600 bg-gradient-to-br from-sandyBeige to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
         <div className="flex flex-row justify-between items-center">
           <div className="px-4 py-4 bg-gray-300 rounded-xl bg-opacity-30">
             <div className="h-6 w-6 flex items-center justify-center">
@@ -9,10 +9,10 @@ export const Card = ({text, className, name}) => {
             </div>
           </div>
         </div>
-        <h1 className="text-3xl sm:text-4xl text-white xl:text-5xl font-bold mt-12">
+        <h1 className="text-3xl sm:text-4xl  xl:text-5xl font-bold mt-12">
           {text}
         </h1>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between font-bold text-black">
           <p>{name}</p>
         </div>
       </div>
--- Frontend/src/pages/Admin/Status.jsx
+++ Frontend/src/pages/Admin/Status.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { DashboardContext } from "../../context/DashboardContext";
+import { Card } from "./Card";
 
 export const Status = () => {
   const { products, orders, admins } = useContext(DashboardContext);
@@ -15,74 +16,23 @@ export const Status = () => {
   return (
     <div class="container items-center px-4 py-8 m-auto mt-5">
       <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
-        <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-          <div class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-sandyBeige hover:to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
-            <div class="flex flex-row justify-between items-center">
-              <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                <div className="h-6 w-6 group-hover:text-gray-50 flex items-center justify-center">
-                  <i className="fa fa-user "></i>
-                </div>
-              </div>
-            </div>
-            <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">
-              {admins.length}
-            </h1>
-            <div class="flex flex-row justify-between group-hover:text-gray-200">
-              <p>Registered Admins</p>
-            </div>
-          </div>
-        </div>
-        <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-          <div class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-sandyBeige hover:to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
-            <div class="flex flex-row justify-between items-center">
-              <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                <div className="h-6 w-6 group-hover:text-gray-50 flex items-center justify-center">
-                  <i className="fa fa-shopping-cart"></i>
-                </div>
-              </div>
-            </div>
-            <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">
-              {products.length}
-            </h1>
-            <div class="flex flex-row justify-between group-hover:text-gray-200">
-              <p>Total Products</p>
-            </div>
-          </div>
-        </div>
-        <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-          <div class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-sandyBeige hover:to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
-            <div class="flex flex-row justify-between items-center">
-              <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                <div className="h-6 w-6 group-hover:text-gray-50 flex items-center justify-center">
-                  <i className="fa fa-check"></i>
-                </div>
-              </div>
-            </div>
-            <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">
-              {orders.length}
-            </h1>
-            <div class="flex flex-row justify-between group-hover:text-gray-200">
-              <p>Total orders</p>
-            </div>
-          </div>
-        </div>
-        <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-          <div class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-sandyBeige hover:to-clayBrown rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
-            <div class="flex flex-row justify-between items-center">
-              <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                <div className="h-6 w-6 group-hover:text-gray-50 flex items-center justify-center">
-                  <i class="fa fa-list"></i>
-                </div>
-              </div>
-            </div>
-            <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">
-              {totalStock}
-            </h1>
-            <div class="flex flex-row justify-between group-hover:text-gray-200">
-              <p>Stock</p>
-            </div>
-          </div>
-        </div>
+        <Card
+          text={admins.length}
+          name="Registered Admins"
+          className="fa fa-user-circle"
+        />
+        <Card
+          text={products.length}
+          name="Total Products"
+          className="fa fa-shopping-cart"
+        />
+
+        <Card
+          text={orders.length}
+          name="Total Orders"
+          className="fa fa-check"
+        />
+        <Card text={totalStock} name="Stock" className="fa fa-list" />
       </div>
     </div>
   );
